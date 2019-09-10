460c528b71416b76194aa646aee00795887966e5
rarrunategu1 <parker.rose@ymail.com>
2019-09-09 20:54:44 -0400

    Notes added to show what each section of my class entails.  Constructor also added

--- Bank.cs
+++ Bank.cs
@@ -2,16 +2,20 @@ using System;
 
 namespace Hello_World{
 
-  public class Bank{
-
+  public class Bank
+  {
+    //Read only Auto-Implemented Property
     public long _acctNumber{get;}
     
+    //Member variable
     public string _location {get; set;}
 
+    //Constructor 
     public Bank(long acctNumber)
     {
       this._acctNumber = acctNumber;
     }
+    //Method
     public void MakeDeposit()
     {
       Console.WriteLine("Your deposit transaction has been completed to account number {0}.", _acctNumber);
