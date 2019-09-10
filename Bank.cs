b97d68ef7a79591d98cfc2c478842d78297ae56b
rarrunategu1 <parker.rose@ymail.com>
2019-09-09 20:51:39 -0400

    added a read only auto-implemented property to get the user's account number and return value when deposit was made

--- Bank.cs
+++ Bank.cs
@@ -4,11 +4,17 @@ namespace Hello_World{
 
   public class Bank{
 
+    public long _acctNumber{get;}
+    
     public string _location {get; set;}
 
+    public Bank(long acctNumber)
+    {
+      this._acctNumber = acctNumber;
+    }
     public void MakeDeposit()
     {
-      Console.WriteLine("Your deposit transaction has been completed.");
+      Console.WriteLine("Your deposit transaction has been completed to account number {0}.", _acctNumber);
     }
 
   }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Bank bank = new Bank();
+            Bank bank = new Bank(123456789);
             bank._location = "Kansas";
 
             Console.WriteLine(bank._location);
