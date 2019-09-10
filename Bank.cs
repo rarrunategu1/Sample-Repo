0870e734197b8f9fafe10930d8eb27f4eecf90dd
rarrunategu1 <parker.rose@ymail.com>
2019-09-10 13:08:52 -0400

    combined my constructors and added if else statements depending on paramaeter input.  Would like for balance to reflect whatever transaction is made each time.  May add a whle statement asking for repetitive user input on transactions to try to make it work.

--- Bank.cs
+++ Bank.cs
@@ -5,10 +5,16 @@ namespace Hello_World{
   public class Bank
   {
     //Private Member Variable
+    private double depositAmount;
     private double withdrawalAmount;
     public double bankFee = 3.00;
-    private double balance = 25.00;
+    private double balance = 0.00;
 
+    //Read only Auto-Implemented Property
+    private long _acctNumber = 123456789;
+    
+    //Member variable
+    public string _location {get; set;}
     public double WithdrawalAmount
     {
       get
@@ -20,27 +26,46 @@ namespace Hello_World{
         withdrawalAmount = value;
       }
     }
-
-    //Read only Auto-Implemented Property
-    public long _acctNumber{get;}
-    
-    //Member variable
-    public string _location {get; set;}
+    public double DepositAmount
+    {
+      get
+      {
+        return depositAmount; 
+      }
+      set
+      {
+        depositAmount = value;
+      }
+    }
 
     //Constructor 
-    public Bank(long acctNumber)
+    public Bank(long acctNumber, double transactionamount, string transactiontype)
     {
-      this._acctNumber = acctNumber;
+      if (this._acctNumber == acctNumber && transactiontype == "withdrawal")
+      {
+        this.WithdrawalAmount = transactionamount;
+        Console.WriteLine("We're happy to assist you with your withdrawal!");
+        MakeWithdrawal();
+      }
+      else if (this._acctNumber == acctNumber && transactiontype == "deposit")
+      {
+        this.depositAmount = transactionamount;
+        Console.WriteLine("We're happy to assist you with your deposit!");
+        MakeDeposit();
+      }
+      if (this._acctNumber != acctNumber)
+      {
+        Console.WriteLine("Sorry that is an incorrect account number.");
+      }
+      //require transaction type here
     }
-    public Bank(double withdrawalamount)
-    {
-      this.WithdrawalAmount = withdrawalamount;
-    }
+    
     
     //Method
     public void MakeDeposit()
     {
-      Console.WriteLine("Your deposit transaction has been completed to account number {0}.", _acctNumber);
+      balance += depositAmount;
+      Console.WriteLine("Your deposit transaction has been made to account number {0}.  Your new balance is ${1:0.00}", _acctNumber, balance);
     }
 
     public void MakeWithdrawal()
@@ -52,7 +77,6 @@ namespace Hello_World{
         Console.WriteLine("Your current balance is ${0:0.00}.  There is a fee of ${1:0.00} for your withdrawal.  Your balance after your withdrawal of ${2:0.00} is ${3:0.00}.", balance, bankFee, withdrawalAmount, (balance - withdrawalAmount- bankFee));
 
         balance = balance - withdrawalAmount - bankFee;
-        Console.WriteLine("New balance is ${0:0.00}", balance);
       }
       else
       {
--- Program.cs
+++ Program.cs
@@ -6,15 +6,13 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Bank bankTrans1 = new Bank(123456789);
-            Bank bankTrans2 = new Bank(21.00);
-            bankTrans1._location = "Kansas";
+            Bank bankTrans = new Bank(123456789, 21.00, "deposit");
+            bankTrans._location = "Kansas";
 
-            Console.WriteLine(bankTrans1._location);
+            //Console.WriteLine(bankTrans1._location);
 
-            bankTrans1.MakeDeposit();
-            bankTrans2.MakeWithdrawal();
-            bankTrans2.MakeWithdrawal();
+            
+            
         }
     }
 }
